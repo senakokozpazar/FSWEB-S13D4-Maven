af08a7b10cd44227d936b3be035b79483df32b69
Sena Kokoz Pazar <senakokoz@gmail.com>
2024-09-19 12:19:43 +0300

    -Point class is created. -Instance variables are created. -Getter and setter methods are created. -Constructor is created.

---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Point(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+}
